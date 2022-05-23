d6db197a63a46c1ccc2cb12ec5186369ce1a0a37
TomLeWagon <tmarwedel@slb.com>
2022-05-23 09:35:56 -0500

    task manager finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # show all tasks
+  # get 'tasks',      to: 'tasks#index'
+  #   # The `new` route needs to be *before*
+  # get "tasks/new",  to: "tasks#new"
+  # post "tasks",     to: "tasks#create"
+  # # show a single task
+  # get "tasks/:id",  to: "tasks#show", as: :task
+  # # edit a task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # # update task
+  # patch "tasks/:id", to: "tasks#update"
+  # # delete
+  # delete "tasks/:id", to: "tasks#destroy"
 
-  get 'tasks', to: 'tasks#show'
+  resources :tasks
 end
